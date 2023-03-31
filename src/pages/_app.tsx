de331feb1ea0e3c532898a1abe8793be2f0c7463
Franzua Plasencia <franzua.15pc@gmail.com>
2023-03-30 20:19:20 -0500

    Finisth Project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
+import React from 'react';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { NextUIProvider } from '@nextui-org/react';
+import { darkTheme } from '../themes';
+
+import '@/styles/globals.css'
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactNode) => React.ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout || ((page: React.ReactNode) => page)
+
+  return (
+    <NextUIProvider theme={darkTheme}>
+      {getLayout(<Component {...pageProps} />)}
+    </NextUIProvider>
+  )
 }
---
+++ src/utils/getPokemonInfo.ts
@@ -0,0 +1,20 @@
+import { ParsedUrlQuery } from "querystring";
+import { Pokemon } from '../interfaces/pokemon';
+import pokeApi from '../api/pokeApi';
+
+
+interface Params extends ParsedUrlQuery {
+  name: string
+}
+
+export const getPokemonInfo = async (nameOrId: string) => {
+
+  const { data } = await pokeApi.get<Pokemon>(`pokemon/${nameOrId}`)
+
+  return {
+    id: data.id,
+    name: data.name,
+    sprites: data.sprites
+  }
+
+}
